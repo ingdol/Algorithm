223524929a9633f443370ef2c4b62df2085d6cc9
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-02-02 15:59:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,5 @@
 function solution(s) {
     let arr = s.split(' ')
-    let sen = arr.map((str, i) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ')
+    let sen = arr.map((str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ')
     return sen
 }
