c605ec9cafa190cec110878f48e65e15f12a39be
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-02-04 23:20:51 +0900

    [level 2] Title: 올바른 괄호, Time: 2.82 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,9 @@
+function solution(s){
+    let sum = 0
+    for(let i = 0; i < s.length; i++) {
+        s[i] === '(' ? sum += 1 : sum += -1
+        if(sum < 0) return false
+    }
+    
+    return sum === 0 ?  true :  false
+}
