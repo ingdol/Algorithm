720b445533786e8a8cbe3c0ca223f953a92e62ae
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-02-19 23:48:37 +0900

    [level 2] Title: 구명보트, Time: 10.66 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/README.md
+++ 프로그래머스/lv2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.7 MB, 시간: 32.40 ms
+메모리: 38.3 MB, 시간: 10.66 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,15 +1,13 @@
 function solution(people, limit) {
-    let cnt = 0;
-    let arr = people.sort((a, b) => a - b)
-    let sum = 0
-    let lt = 0;
+    let arr = people.sort((a, b) => a - b);
+    let cnt = sum = lt = 0;
     let rt = arr.length - 1;
     while (lt < rt) {
         sum = arr[lt] + arr[rt]
-        if(sum <= limit) lt += 1;
-        rt -= 1;
-        cnt += 1;
+        if(sum <= limit) lt++;
+        rt--;
+        cnt++;
     }
-    if (rt === lt) cnt += 1;
+    if (rt === lt) cnt++;
     return cnt;
 }
