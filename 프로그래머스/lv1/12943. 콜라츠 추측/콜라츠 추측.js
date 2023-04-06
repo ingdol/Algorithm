d40605eb5834837fb95bb1c250a8df769c18e413
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-04-06 15:18:08 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    let answer = 0
+    while(num !== 1) {
+        if(num % 2 === 0) num /= 2
+        else num = num * 3 + 1
+        answer++
+    }
+    return answer >= 500 ? -1 : answer
+}
