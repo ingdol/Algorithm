66b8c3440d330d3b350299abffab1529fface0bc
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-05-03 23:17:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.93 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,13 @@
+function solution(arr)
+{
+    let answer = [];
+    
+    let i = 0;
+    while (i < arr.length) {
+        if(arr[i] !== arr[i+1]) {
+            answer.push(arr[i])
+        }
+        i++
+    }
+    return answer;
+}
