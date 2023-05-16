4c1cbace41bb8a6059010aa1fbdeb0af5c66bbe8
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-05-16 09:07:12 +0900

    [unrated] Title: 배열 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -2,10 +2,8 @@ function solution(arr1, arr2) {
     if(arr1.length > arr2.length) return 1
     else if(arr1.length < arr2.length) return -1
     else {
-        let sum1 = 0
-        arr1.map((x) => sum1 += x)
-        let sum2 = 0
-        arr2.map((y) => sum2 += y)
+        let sum1 = arr1.reduce((x, y) => x + y)
+        let sum2 = arr2.reduce((x, y) => x + y)
         
         if(sum1 > sum2) return 1
         else if(sum1 < sum2) return -1
