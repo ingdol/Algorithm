affabc2e47130533cc6c30d46391b8876ae029f5
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-04 11:18:34 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,17 +1,17 @@
 function solution(n) {
-    let answer = 0
-    let trit = []
-    while(n >= 1) { 
-        trit.push(n%3)
-        n = Math.floor(n/3)
-    }
-    trit.reverse()
-    let threeNum = 1
-    trit.map((t, i) => 
-         {
-            answer +=  t * threeNum,
-            threeNum *= 3 
-        }  
-    )
-    return answer;
+    // let answer = 0
+    // let trit = []
+    // while(n >= 1) { 
+    //     trit.push(n%3)
+    //     n = Math.floor(n/3)
+    // }
+    // trit.reverse()
+    // let threeNum = 1
+    // trit.map((t, i) => 
+    //      {
+    //         answer +=  t * threeNum,
+    //         threeNum *= 3 
+    //     }  
+    // )
+    return parseInt([...n.toString(3)].reverse().join(''), 3);
 }
