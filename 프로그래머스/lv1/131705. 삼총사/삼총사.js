a63dba975ff18108991a26f78aeaedb93139db06
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-08 13:58:32 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,15 +1,12 @@
 function solution(number) {
-    let answer = 0, lf = 0;
-    while(lf <= number.length - 3) {
-        let rf = lf + 1
-        while(rf < number.length - 1) {
+    let answer = 0;
+    for(let lf = 0; lf < number.length - 2; lf++) {
+        for(let rf = lf + 1; rf < number.length - 1; rf++) {
             for(let i = rf + 1; i < number.length; i++) {
                 let sum = number[lf] + number[rf] + number[i]
                 if(sum === 0) answer++
             }
-            rf++
         }
-        lf++
     }
     return answer;
 }
