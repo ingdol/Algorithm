2c3bd78de5fd807492ae0b4a2aca5dde37e22080
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-11 11:12:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 5.80 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,8 +1,18 @@
 function solution(s) {
-    const dic = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    dic.map((d, i) => {
-        const regexString = new RegExp(d, 'gi')
-        s = s.replace(regexString, i)
-    })
-    return Number(s);
+    const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    let answer = s
+    
+    
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        console.log(arr)
+        answer = arr.join(i);
+        console.log(answer)
+    }
+
+    // numbers.map((n, i) => {
+    //     const regexString = new RegExp(n, 'gi')
+    //     s = s.replace(regexString, i)
+    // })
+    return Number(answer);
 }
