431ab34b9b8d1f4882ec3f926dec96d895c8872a
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-14 14:34:38 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+for (let i = 1; i <= input[0]; i++) {
+  let arr = input[i].split(' ');
+  console.log(`Case #${i}: ` + (Number(arr[0]) + Number(arr[1])));
+}
