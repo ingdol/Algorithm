b350eea2917f1454b3e66590f7b32eb69956f4a7
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-14 14:30:56 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,5 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+for (let i = 0; i < input.length - 1; i++) {
+  let arr = input[i].split(' ');
+  console.log(Number(arr[0]) + Number(arr[1]));
+}
