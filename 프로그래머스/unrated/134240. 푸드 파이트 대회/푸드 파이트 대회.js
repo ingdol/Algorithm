fd334443b2c2bbde87f8d3b98167d800abd1c8f8
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-21 22:45:11 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,5 +1,11 @@
 function solution(food) {
-    var answer = '';
-    console.log(food)
-    return answer;
+    let answer = '';
+    for(let i = 1; i < food.length; i++) {
+        let num = Math.floor(food[i] / 2)
+        while(num > 0) {
+            answer += i
+            num--
+        }
+    }
+    return answer + '0' + [...answer].reverse().join("");
 }
