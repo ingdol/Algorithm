eb10a4407b5ffcce91de137c67018399f74a207f
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-24 16:53:47 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.39 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,19 +1,26 @@
 function solution(s) {
-    let answer = [];
-    let arr = [...s];
-    let putArr = [];
-    arr.map((str, i) => {
-        if(putArr.indexOf(str) === -1) {
-            answer.push(-1)
-        } else {
-            let index = 0
-            putArr.map((c, j) => {
-                if (c === str) index = i - j
-            })
-            answer.push(index)
-        }
-        putArr.push(str)
+    const hash={};
+
+    return [...s].map((v,i)=>{
+        let result = hash[v] !== undefined ? i - hash[v] : -1;
+        hash[v] = i;
+        return result;
+    });
+//     let answer = [];
+//     let arr = [...s];
+//     let putArr = [];
+//     arr.map((str, i) => {
+//         if(putArr.indexOf(str) === -1) {
+//             answer.push(-1)
+//         } else {
+//             let index = 0
+//             putArr.map((c, j) => {
+//                 if (c === str) index = i - j
+//             })
+//             answer.push(index)
+//         }
+//         putArr.push(str)
         
-    })
-    return answer;
+//     })
+//     return answer;
 }
