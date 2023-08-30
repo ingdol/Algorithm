222db6c67818725122c77eba2ade489f880baec7
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-08-30 11:01:59 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,20 @@
+function solution(nums) {
+    let answer = 0;
+    
+    for(let i = 0; i < nums.length - 2; i++) {
+        for(let j = i + 1; j < nums.length - 1; j++) {
+            for(let l = j + 1; l < nums.length; l++) {
+                if(isPrime(nums[i] + nums[j] + nums[l])) answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
+
+function isPrime(num) {
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(num % i === 0) return false;
+    }
+    return true;
+}
