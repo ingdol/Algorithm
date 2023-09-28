ee4d5fd4d675d033551e78e6810c30af7909a231
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-09-29 01:51:40 +0900

    [Silver III] Title: 소수 구하기, Time: 4144 ms, Memory: 17096 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const isPrime = (num) => {
+    if(num === 1) return false;
+    for(var i=2; i<=Math.sqrt(num); i++){
+        if((num%i)==0) return false;
+    }
+    return true;
+}
+
+const [n,m] = input.shift().split(' ').map(e=>parseInt(e));
+
+for(var i=n; i<=m; i++) 
+    isPrime(i) ? console.log(i) : null;
