9fff64fafede84a03e92b632fa92609c358df911
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-10-30 23:50:31 +0900

    [Silver IV] Title: 접미사 배열, Time: 128 ms, Memory: 10440 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+let arr = [];
+for (let i = 0; i < input.length; i++) {
+  arr.push(input.slice(i));
+}
+console.log(arr.sort().join("\n"));
