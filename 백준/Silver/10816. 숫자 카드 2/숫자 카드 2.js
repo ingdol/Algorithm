23f40f788727f0af2cf1d677d9db4c78d57b71bd
eunji Shin <65396646+ingdol@users.noreply.github.com>
2023-12-12 22:31:37 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 656 ms, Memory: 130152 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,15 +1,11 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().trim().split(/\n/); 
+let input = require("fs").readFileSync("/dev/stdin").toString().trim().split(/\n/);
 
 const haveCard = input[1].split(" ").map(Number);
 
-let cardDic = new Map();
-haveCard.forEach(n => {
-  if (cardDic.has(n)) {
-    cardDic.set(n, cardDic.get(n) + 1);
-  } else cardDic.set(n, 1);
-});
-
-const checkCard = input[3].split(" ").map(Number);
-let resultArr = checkCard.map(m => (cardDic.has(m) ? cardDic.get(m) : 0));
-
-console.log(resultArr.join(" "));
+let cardArr = new Map();
+haveCard.forEach(n =>
+  cardArr.has(n) ? cardArr.set(n, cardArr.get(n) + 1) : cardArr.set(n, 1)
+);
+const compareCard = input[3].split(" ").map(Number);
+const res = compareCard.map(m => cardArr.get(m) || 0);
+console.log(res.join(" "));
