269bd7c7f2b8a49343004d532e63f08971e1edce
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-09-05 14:45:44 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+def fib(n):
+    a, b = 1, 1
+    for _ in range(3, n + 1):
+        a, b = b, a + b
+    return b
+
+def fibonacci(n):
+    return n - 2
+
+print(fib(N), fibonacci(N))
