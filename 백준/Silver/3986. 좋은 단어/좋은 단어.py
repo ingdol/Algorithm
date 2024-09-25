5e0e24fdcb89265eac52565aee54c13a179c5c8d
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-09-25 16:12:05 +0900

    [Silver IV] Title: 좋은 단어, Time: 92 ms, Memory: 31552 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.py
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -3,15 +3,16 @@ input = sys.stdin.readline
 
 N = int(input())
 res = 0
+
 for _ in range(N):
-    str = input().split()[0]
-    stack = [str[0]]
-    for i in range(1, len(str)):
-        if stack:
-            if stack[-1] == str[i]:
-                stack.pop() 
-            else: stack.append(str[i])
-        else: stack.append(str[i])
-    if not len(stack): res += 1
-    
+    word = input().strip()
+    stack = []
+    for char in word:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    if not stack:
+        res += 1
+
 print(res)
