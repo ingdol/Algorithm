92017947c369c3dcf58df5ffbb720cb1afa9cee2
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-09-27 14:20:37 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-dic = {}
+key_list = []
 for _ in range(N):
-    dic[input().strip()] = 0
-key_list = dic.keys()
+    key_list.append(input().strip())
 
 for key in key_list:
     reverse_key = key[::-1]
-    if reverse_key in dic:
+    if reverse_key in key_list:
         key_length = len(reverse_key)
         print(key_length, reverse_key[key_length//2])
         break
