c67575b44fe31f9388ff2a0dff24262cb064849a
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-09-30 20:45:20 +0900

    [Silver IV] Title: 상근이의 여행, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,7 +1,11 @@
-T = int(input())  # 테스트 케이스 수 입력
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
 
 for _ in range(T):
-    N, M = map(int, input().split())  # 국가 수 N과 비행기 종류 M 입력
+    N, M = map(int, input().split())
     for _ in range(M):
-        pass_line = input()
-    print(N - 1)  # 최소 간선 수는 항상 N - 1
+        input()
+    print(N - 1)
