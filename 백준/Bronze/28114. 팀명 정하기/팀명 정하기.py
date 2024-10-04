326887ccf3e8937a49fc8f42b1f68ba7c3b46ccb
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-10-04 11:00:15 +0900

    [Bronze II] Title: 팀명 정하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
+++ 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
@@ -1,16 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-# 입력 받기
-data = [input().split() for _ in range(3)]
-participants = [(int(P), int(Y), S) for P, Y, S in data]
+Y_list = []
+P_S_list = []
 
-# 첫 번째 방법: 입학 연도를 100으로 나눈 나머지를 오름차순으로 정렬한 후 이어 붙이기
-team_name_1 = ''.join(map(str, sorted([Y % 100 for _, Y, _ in participants])))
+for _ in range(3):
+    P, Y, S = input().strip().split()
+    Y_list.append(int(Y))
+    P_S_list.append((int(P), S))
 
-# 두 번째 방법: 해결한 문제 수를 기준으로 내림차순 정렬한 후 성씨의 첫 글자를 붙이기
-team_name_2 = ''.join([S[0] for _, _, S in sorted(participants, key=lambda x: -x[0])])
+result_1 = sorted(Y_list)
+result_1 = list(map(lambda y : y % 100, result_1))
+result_1 = ''.join(map(str, result_1))
+print(result_1)
 
-# 결과 출력
-print(team_name_1)
-print(team_name_2)
+result_2 = sorted(P_S_list, reverse=True)
+result_2 = list(map(lambda list : list[1][0], result_2))
+result_2 = ''.join(result_2)
+print(result_2)
