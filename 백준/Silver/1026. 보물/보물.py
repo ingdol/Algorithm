a25bd6cd87e2dc7392016aff9ac485e5f281a806
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-10-04 11:57:44 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = sorted(list(map(int, input().split())))
+B = sorted(list(map(int, input().split())), reverse=True)
+
+result = 0
+for i in range(N):
+    result += A[i] * B[i]
+
+print(result)
