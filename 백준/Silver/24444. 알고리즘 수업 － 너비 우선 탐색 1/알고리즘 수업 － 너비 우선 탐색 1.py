cd93e35bf7527d8b1559bbd01cfaac03dfb3cd13
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-10-07 12:00:40 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 520 ms, Memory: 58984 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,31 +1,32 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-def bfs(node):
-    queue = [node]
-    visited[node] = 1
-    order_count = 2
+def bfs(R):
+    queue = deque([R])
+    visited[R] = 1
+    cnt = 1
     while queue:
-        cur = queue.pop(0)
-        for neighbor in graph[cur]:
-            if visited[neighbor] == 0:
-                queue.append(neighbor)
-                visited[neighbor] = order_count
-                order_count += 1
+        node = queue.popleft()
+        for inner_node in graph[node]:
+            if visited[inner_node] == 0:
+                cnt += 1
+                visited[inner_node] = cnt
+                queue.append(inner_node)
 
-N, M, R = list(map(int, input().split()))
+N, M, R = map(int, input().split())
+
 graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
-    u, v = list(map(int, input().split()))
+    u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-for i in range(1, N + 1): 
-    graph[i] = sorted(graph[i])
-
-visited = [0]*(N+1)
-
+for i in range(1, N + 1):
+    graph[i].sort()
+    
+visited = [0 for _ in range(N + 1)]
 bfs(R)
 
 for i in range(1, N + 1):
