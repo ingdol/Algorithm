9781b0f23700b4593406ca6f6302550b1a254211
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-10-07 11:40:56 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 524 ms, Memory: 70380 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,29 +1,28 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-def dfs(node, order):
-    visited[node] = order
+def dfs(R, cnt):
+    visited[R] = cnt
     order_count[0] += 1
-    
-    for neighbor in graph[node]:
-        if visited[neighbor] == 0:
-            dfs(neighbor, order_count[0])
+    for inner_node in graph[R]:
+        if visited[inner_node] == 0:
+            dfs(inner_node, order_count[0])
 
-N, M, R = list(map(int, input().split()))
+N, M, R = map(int, input().split())
+
 graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
-    u, v = list(map(int, input().split()))
+    u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
 for i in range(1, N + 1):
-    graph[i] = sorted(graph[i])
+    graph[i].sort()
 
-visited = [0]*(N+1)
+visited = [0 for _ in range(N + 1)]
 order_count = [1]
-
 dfs(R, 1)
 
 for i in range(1, N + 1):
