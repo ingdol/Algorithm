94c2c37ea8890c57137e321436a7db9f4eb6e58f
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-10-08 22:56:37 +0900

    [Silver I] Title: 나이트의 이동, Time: 988 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -2,32 +2,30 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-move_coordinate = [[-1, 2], [1, 2], [-2, 1], [2, 1], [-2, -1], [2, -1], [-1, -2], [1, -2]]
+directions = [(-1, 2), (1, 2), (-2, 1), (2, 1), (-2, -1), (2, -1), (-1, -2), (1, -2)]
 
 def bfs(start, end, l):
-    if start == end:
-        return 0
+    visited = [[False for _ in range(l)] for _ in range(l)]
     queue = deque([[start[0], start[1], 0]])
-    visitied[start[0]][start[1]] = 1
-    
-    while queue:
-        stand_node = queue.popleft()
-        x, y, cnt = stand_node
-        for move in move_coordinate:
-            move_x = x + move[0]
-            move_y = y + move[1]
-            
-            if end[0] == move_x and end[1] == move_y:
-                return cnt + 1
-            if 0 <= move_x < l and 0 <= move_y < l and visitied[move_x][move_y] == 0:
-                visitied[move_x][move_y] = 1
-                queue.append([move_x, move_y, cnt + 1])
-        
+    visited[start[0]][start[1]] = True
 
+    while queue:
+        cur_x, cur_y, cur_cnt = queue.popleft()
+
+        if [cur_x, cur_y] == end:
+            return cur_cnt
+
+        for direction in directions:
+            move_x = cur_x + direction[0]
+            move_y = cur_y + direction[1]
+            
+            if 0 <= move_x < l and 0 <= move_y < l and not visited[move_x][move_y]:
+                visited[move_x][move_y] = True
+                queue.append([move_x, move_y, cur_cnt + 1])
+        
 T = int(input())
 for _ in range(T):
     l = int(input())
-    visitied = [[0 for _ in range(l)] for _ in range(l)]
     start = list(map(int, input().split()))
     end = list(map(int, input().split()))
     print(bfs(start, end, l))
