68cb023408e65434fdd9532e1ccda416c2d2bbb9
eunji Shin <65396646+ingdol@users.noreply.github.com>
2025-03-18 22:43:24 +0900

    [level 2] Title: 방문 길이, Time: 7.69 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,23 @@
+function solution(dirs) {
+    const dirs_dic = {
+        'U': [0, 1],
+        'D': [0, -1],
+        'R': [1, 0],
+        'L': [-1, 0]
+    }
+    let visited = []
+    let start_dir = [0, 0]
+    for (let dir of dirs) {
+        const [x, y] = dirs_dic[dir] 
+        const [cur_x, cur_y] = [start_dir[0] + x, start_dir[1] + y]
+        if ((cur_x >= -5 && cur_x <= 5) && (cur_y >= -5 && cur_y <= 5)) {
+            visited.push([start_dir[0], start_dir[1], cur_x, cur_y])
+            visited.push([cur_x, cur_y, start_dir[0], start_dir[1]])
+            start_dir = [cur_x, cur_y]
+        }
+    }
+    const answer = Array.from(new Set(visited.map(a => JSON.stringify(a))), json => JSON.parse(json))
+    console.log(answer)
+    return answer.length / 2
+    
+}
