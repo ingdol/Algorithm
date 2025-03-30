678a87c949e8eb55a630a44b98cc0588fe93fe14
eunji Shin <65396646+ingdol@users.noreply.github.com>
2025-03-30 15:29:59 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.97 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,10 +1,14 @@
 function solution(s)
 {
-    let str = [];
-    
-    for(let i = 0; i < s.length; i++) {
-        if (!str.length || str[str.length - 1] !== s[i]) str.push(s[i]) 
-        else str.pop(s[i])
+    let stack = []
+    for (let c of s) {
+        if (stack[stack.length - 1] === c) {
+            stack.pop()
+        } else {
+            stack.push(c)
+        }
     }
-    return str.length === 0 ? 1 : 0
+
+
+    return stack.length > 0 ? 0 : 1;
 }
