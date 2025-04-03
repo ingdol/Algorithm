e0ccc7b5bb6403510fb4964716d562578b86b5c9
eunji Shin <65396646+ingdol@users.noreply.github.com>
2025-04-03 23:13:59 +0900

    [level 2] Title: 오픈채팅방, Time: 330.44 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,19 +1,18 @@
 function solution(record) {
     let answer = [];
-    let checkList = []
     let nameList = {}
     for (let text of record) {
         let textArr = text.split(' ')
-        checkList.push([textArr[0], textArr[1]])
         if (textArr[0] !== 'Leave') {
             nameList[textArr[1]] = textArr[2]
         }
     }
-    for (let check of checkList) {
-        if (check[0] === 'Enter') {
-            answer.push(`${nameList[check[1]]}님이 들어왔습니다.`)
-        } else if (check[0] === 'Leave') {
-            answer.push(`${nameList[check[1]]}님이 나갔습니다.`)
+    for (let text of record) {
+        let textArr = text.split(' ')
+        if (textArr[0] === 'Enter') {
+            answer.push(`${nameList[textArr[1]]}님이 들어왔습니다.`)
+        } else if (textArr[0] === 'Leave') {
+            answer.push(`${nameList[textArr[1]]}님이 나갔습니다.`)
         }
     }
     return answer;
